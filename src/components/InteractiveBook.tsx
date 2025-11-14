71d2a5c2122849ef95f7f97d80dde8f08364c79e
Naldo-12 <89747208+Naldo-12@users.noreply.github.com>
2025-11-14 09:36:58 -0500

    Updated InteractiveBook.tsx

--- src/components/InteractiveBook.tsx
+++ src/components/InteractiveBook.tsx
@@ -21,7 +21,7 @@ export default function InteractiveBook() {
     },
     {
       title: 'Table of Contents',
-      content: 'Vision Statement........................................................iv',
+      content: 'Vision Statement..............................................iv',
       isCover: false,
     },
     {
